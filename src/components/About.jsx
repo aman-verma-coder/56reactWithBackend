a4b088f3f944ca6ee8715dbd27ab1f7c318d669c
Aman Verma <9330nama@gmail.com>
2024-06-10 20:53:37 +0530

    Call Hook

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 function About() {
+    const navigate=useNavigate();
+    function clickHandler() {
+        navigate("/labs");
+    }
     return(
         <div>
-            About
+            <div>
+                About
+            </div>
+            <button onClick={clickHandler}>Labs</button>
         </div>
     )
 }
